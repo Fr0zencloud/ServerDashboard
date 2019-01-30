d0efd8e78ef7fe49f3251d8cb888398b7b8cdcdb
Niklas Lausch <31246997+Niki2k1@users.noreply.github.com>
2019-01-30 18:37:02 +0100

    Init

---
+++ DashboardHelper/DashboardHelper/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+
+namespace DashboardHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebServer ws = new WebServer(SendResponse, "http://localhost:8080/system/infos/");
+            ws.Run();
+            Console.WriteLine("A simple webserver. Press a key to quit.");
+            Console.ReadKey();
+            ws.Stop();
+        }
+
+        public static string SendResponse(HttpListenerRequest request)
+        {
+            string OSinfo = System.Runtime.InteropServices.RuntimeInformation.OSDescription;
+            string response = "{" + OSinfo + "}";
+            return response;
+        }
+    }
+}
