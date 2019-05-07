c69dd88cc609fc1f30f2b6d983b27708ee0f634a
Niklas Lausch <support@niki2k1.de>
2019-05-07 10:04:03 +0200

    Added Disk usage

--- DashboardHelper/index.js
+++ DashboardHelper/index.js
@@ -1,36 +1,58 @@
 const os = require('os')
+const disk = require('diskusage')
 const express = require('express')
-let app = express();
+let app = express()
+//https://www.npmjs.com/package/cpu-stat
 
-let cpu = os.cpus()
 let cpuName = os.cpus()[0].model.replace('(R)', '').replace('(TM)', '').replace(' CPU', '')
-let arch = os.arch()
-let freeMemGb = (os.freemem() / 1000000000).toFixed(2)
-let hostname = os.hostname()
+let diskFree, diskTotal, platform
+
+switch(os.platform()) {
+    case "darwin":
+        platform = "MacOS"
+        break;
+    case "win32":
+        platform = "Windows"
+        break;
+    case "linux":
+        platform = "Linux"
+        break;
+}
+
+disk.check('/', function(err, info) {
+    if (err) {
+      console.log(err);
+    } else {
+        diskFree = info.free
+        diskTotal = info.total
+    }
+  });
 
 let machine = {
     hostname: os.hostname(),
     cpu: os.cpus().length + ' x ' + cpuName,
-    platform: os.platform(),
+    platform: platform,
     arch: os.arch(),
     totalMemory: (os.totalmem() / 1000000000).toFixed(2),
     freeMemory: (os.freemem() / 1000000000).toFixed(2),
+    diskFree: (diskFree / 1000000000).toFixed(2),
+    diskTotal: (diskTotal / 1000000000).toFixed(2)
 }
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
     next();
   });
 
 app.get('/system/info/', function (req, res) {
     res.format({
         'application/json': function () {
-            res.send(machine);
+            res.send(machine)
         },
     });
 });
 
 app.listen(3000, function () {
-    console.log('[ServerDashboard] Server is listening on 3000');
+    console.log('[ServerDashboard] Server is listening on 3000')
 });
